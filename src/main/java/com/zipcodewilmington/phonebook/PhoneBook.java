7954cf977aaee64dc7720b115ccd8d6c835f84be
Eric Jefferson <ejefferson50@gmail.com>
2019-02-17 21:22:17 -0500

    hit a wall getting close

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,7 +1,93 @@
 package com.zipcodewilmington.phonebook;
 
+
+
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.TreeMap;
+
+import static com.sun.tools.internal.xjc.reader.Ring.add;
+
 /**
  * Created by leon on 1/23/18.
  */
-public class PhoneBook {
+public class PhoneBook<name> {
+    TreeMap<String, ArrayList> phoneBook = new TreeMap<>();
+
+
+    //TreeMap<String, String> phoneBook = new TreeMap<>();
+    String name;
+    ArrayList phoneNumber;
+
+
+
+
+    public void addEntry(String name, String phoneNumber) {
+
+        if(phoneBook.get(name) == null) {
+            ArrayList<String> phoneNumberList = new ArrayList<>();
+            phoneNumberList.add(phoneNumber);
+
+            phoneBook.put(name, phoneNumberList);
+
+        }else if (phoneBook.get(name)!= null) {
+            for (Map.Entry<String, ArrayList> entry : phoneBook.entrySet()) {
+                if (entry.getKey().equals(name)) {
+                    entry.getValue().add(phoneNumber);
+                    phoneBook.get(name).add(phoneNumber);
+
+                }
+            }
+
+        }
+    }
+
+    public void removeEntry(String name) {
+
+        phoneBook.remove(name);
+
+
+    }
+
+    public String lookupEntry(String name) {
+        if (phoneBook.get(name) != null) {
+            ArrayList<String> phoneNumber = phoneBook.get(name);
+            String result = phoneNumber.toString().replace("[", "").replace("]", "");
+            return result;
+        }else {
+            return null;
+        }
+
+    }
+
+
+
+    public String reverseLookup(String phoneNumber) {
+        String foundName = "";
+
+        for (Map.Entry<String, ArrayList> entry : phoneBook.entrySet()) {
+            for (int index = 0; index < entry.getValue().size(); index++) {
+                if (entry.getValue().indexOf(phoneNumber) != -1) {
+                    name = entry.getKey();
+                }
+            }
+
+
+        }
+        return name;
+    }
+
+     public void display(){
+
+        String result = "";
+
+
+
+
+        }
+
+        System.out.println(phoneBook.values());
+
+
+    }
 }
--- src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
+++ src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
@@ -1,7 +1,103 @@
+
 package com.zipcodewilmington.phonebook;
+
+import org.junit.jupiter.api.Test;
+import org.junit.Assert;
+
+import java.util.Map;
 
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBookTest {
+
+    String name = "Eric Jefferson";
+    String phoneNumber = "215-478-7247";
+
+    @Test
+    public void testAddEntry(){
+        //given
+        PhoneBook phoneBook = new PhoneBook();
+        String name = "Eric Jefferson";
+        String phoneNumber = "215-478-7247";
+
+
+        //when
+        phoneBook.addEntry(name, phoneNumber);
+        String actual = phoneBook.lookupEntry(name);
+        String expected = phoneNumber;
+
+        //then
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testRemoveEntry(){
+        //given
+        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.addEntry(name, phoneNumber);
+
+
+        //when
+        phoneBook.removeEntry(name);
+
+
+
+        //then
+        Assert.assertNull(phoneBook.lookupEntry(name));
+
+    }
+
+    @Test
+    public void testLookupEntry(){
+        //given
+        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.addEntry(name, phoneNumber);
+
+        //when
+        phoneBook.lookupEntry(name);
+
+
+        //then
+        Assert.assertEquals(phoneNumber, phoneBook.lookupEntry(name));
+
+
+    }
+
+    @Test
+    public void testReverseLookup(){
+
+        //given
+        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.addEntry(name, phoneNumber);
+
+
+        //when
+        phoneBook.reverseLookup(phoneNumber);
+
+        //then
+        Assert.assertEquals(name, phoneBook.reverseLookup(phoneNumber));
+
+    }
+
+    @Test
+    public void testDisplay(){
+
+        //given
+        PhoneBook phoneBook = new PhoneBook();
+
+        //when
+        phoneBook.addEntry("Eric Jefferson", "215-478-7247");
+        phoneBook.addEntry("Joe Smoe", "215-555-1111");
+        phoneBook.addEntry("Negative Nancy", "267-555-2222");
+        phoneBook.addEntry("John Doe", "484-333-9999");
+        phoneBook.addEntry("Robin Hood", "267-555-8888");
+
+        //then
+        Assert.assertEquals(name, phoneNumber);
+
+
+
+    }
 }
